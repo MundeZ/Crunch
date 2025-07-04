b08be2550905e364ad090cf0042e0bf586a42fc7
Roman <meynewo@gmail.com>
2025-07-05 00:23:19 +0700

    Add camera setup for ACPlayerCharacter and refine ACCharacter setup

--- Source/Crunch/Private/Player/CPlayerCharacter.cpp
+++ Source/Crunch/Private/Player/CPlayerCharacter.cpp
@@ -2,4 +2,14 @@
 
 
 #include "Player/CPlayerCharacter.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
 
+ACPlayerCharacter::ACPlayerCharacter()
+{
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+
+	ViewCam = CreateDefaultSubobject<UCameraComponent>(TEXT("ViewCam"));
+	ViewCam->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+}
