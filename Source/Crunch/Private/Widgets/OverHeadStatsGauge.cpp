036e43c98fc8e22a1a03472f35f52860af6551a2
Roman <meynewo@gmail.com>
2025-09-21 21:26:01 +0700

    add the over head status gauge

---
+++ Source/Crunch/Private/Widgets/OverHeadStatsGauge.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Widgets/OverHeadStatsGauge.h"
+
+#include "ValueGauge.h"
+#include "GAS/CAttributeSet.h"
+
+void UOverHeadStatsGauge::ConfigureWithASC(class UAbilitySystemComponent* AbilitySystemComponent)
+{
+	if (AbilitySystemComponent)
+	{
+		HealthBar->SetAndBoundToGameplayAttribute(AbilitySystemComponent, UCAttributeSet::GetHealthAttribute(),
+		                                          UCAttributeSet::GetMaxHealthAttribute());
+		ManaBar->SetAndBoundToGameplayAttribute(AbilitySystemComponent, UCAttributeSet::GetManaAttribute(),
+		                                        UCAttributeSet::GetMaxManaAttribute());
+	}
+}
