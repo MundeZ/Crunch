5cd03130a052b549b728a0f238bd47006ed67a30
Roman <meynewo@gmail.com>
2026-02-19 22:14:54 +0700

    116 calculate the total rewards

--- Source/Crunch/Private/GAS/GAP_Dead.cpp
+++ Source/Crunch/Private/GAS/GAP_Dead.cpp
@@ -1,8 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "GAS/GAP_Dead.h"
 
+#include "AbilitySystemComponent.h"
+#include "CHeroAttributeSet.h"
 #include "Engine/OverlapResult.h"
 #include "GAS/CAbilitySystemStatics.h"
 
@@ -12,43 +13,55 @@ UGAP_Dead::UGAP_Dead()
 	FAbilityTriggerData TriggerData;
 	TriggerData.TriggerSource = EGameplayAbilityTriggerSource::GameplayEvent;
 	TriggerData.TriggerTag = UCAbilitySystemStatics::GetDeadStatTag();
-	
+
 	AbilityTriggers.Add(TriggerData);
-	
+
 	ActivationBlockedTags.RemoveTag(UCAbilitySystemStatics::GetStunStatTag());
 }
 
 void UGAP_Dead::ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
-	const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData)
+                                const FGameplayAbilityActivationInfo ActivationInfo,
+                                const FGameplayEventData* TriggerEventData)
 {
 	if (K2_HasAuthority())
 	{
 		AActor* Killer = TriggerEventData->ContextHandle.GetEffectCauser();
-		if (Killer)
+		if (!Killer || UCAbilitySystemStatics::IsHero(Killer))
 		{
-			UE_LOG(LogTemp, Log, TEXT("Killer: %s"), *Killer->GetName());
+			Killer = nullptr;
 		}
-		
+
 		TArray<AActor*> RewardTargets = GetRewardTarget();
-		for (AActor* RewardTarget : RewardTargets)
+		if (RewardTargets.Num() == 0 && !Killer)
 		{
-			UE_LOG(LogTemp, Log, TEXT("Find reward target: %s"), *RewardTarget->GetName());
+			K2_EndAbility();
+			return;
 		}
+
+		if (Killer && RewardTargets.Contains(Killer))
+		{
+			RewardTargets.Add(Killer);
+		}
+		bool bFound = false;
+		float SelfExperience = GetAbilitySystemComponentFromActorInfo_Ensured()->GetGameplayAttributeValue(UCHeroAttributeSet::GetExperienceAttribute(), bFound);
+		float TotalExperienceReward = BaseExperienceReward + ExperienceRewardPerExperience * SelfExperience;
+		float TotalGoldReward = BaseGoldReward + GoldRewardPerExperience * SelfExperience;
+		
 	}
 }
 
 TArray<AActor*> UGAP_Dead::GetRewardTarget() const
 {
 	TSet<AActor*> OutActors;
-	
+
 	AActor* AvatarActor = GetAvatarActorFromActorInfo();
 	if (!AvatarActor || !GetWorld())
 	{
 		return OutActors.Array();
 	}
-	
+
 	FCollisionObjectQueryParams ObjectQueryParams;
-	ObjectQueryParams.AddObjectTypesToQuery(ECC_Pawn); 
+	ObjectQueryParams.AddObjectTypesToQuery(ECC_Pawn);
 	FCollisionShape CollisionShape;
 	CollisionShape.SetSphere(RewardRange);
 
@@ -60,7 +73,8 @@ TArray<AActor*> UGAP_Dead::GetRewardTarget() const
 		{
 			const IGenericTeamAgentInterface* OtherTeamInterface = Cast<IGenericTeamAgentInterface>(
 				OverlapResult.GetActor());
-			if (!OtherTeamInterface || OtherTeamInterface->GetTeamAttitudeTowards(*AvatarActor) != ETeamAttitude::Hostile)
+			if (!OtherTeamInterface || OtherTeamInterface->GetTeamAttitudeTowards(*AvatarActor) !=
+				ETeamAttitude::Hostile)
 			{
 				continue;
 			}
@@ -73,6 +87,6 @@ TArray<AActor*> UGAP_Dead::GetRewardTarget() const
 			OutActors.Add(OverlapResult.GetActor());
 		}
 	}
-	
+
 	return OutActors.Array();
 }
--- Source/Crunch/Private/GAS/GAP_Dead.h
+++ Source/Crunch/Private/GAS/GAP_Dead.h
@@ -13,13 +13,32 @@ UCLASS()
 class UGAP_Dead : public UCGameplayAbility
 {
 	GENERATED_BODY()
+
 public:
 	UGAP_Dead();
-	
-	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+	                             const FGameplayAbilityActivationInfo ActivationInfo,
+	                             const FGameplayEventData* TriggerEventData) override;
+
 private:
 	UPROPERTY(EditDefaultsOnly, Category = "Reward")
 	float RewardRange = 1000.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Reward")
+	float BaseExperienceReward = 200.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Reward")
+	float BaseGoldReward = 200.f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Reward")
+	float ExperienceRewardPerExperience = 0.1f;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Reward")
+	float GoldRewardPerExperience = 0.05f;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Reward")
+	float KillerRewardPortion = 0.5f;
 	
 	TArray<AActor*> GetRewardTarget() const;
 };
